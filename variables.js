3460c62b3dfe031a8dd30a6647c5b16869da81fa
prashanth chowdary <prashanthchowdary@prashanths-MacBook-Air.local>
2023-03-20 16:24:42 +0530

    let keyword is added

--- variables.js
+++ variables.js
@@ -5,3 +5,18 @@ In javascript most of the time we work with data, the "Variable" -> is a "named 
 which means we store the data in our own naming convention 
 
 */
+
+// => => to use the Variable in the javascrit use the "let" keyword
+
+let name = "Hello, World!";
+console.log(name) 
+
+// (or)
+
+let message;
+message = "hi, bob it is good to see you!";
+console.log(message)
+
+//output :  "Hello, World!"
+
+
