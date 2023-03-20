2b88f1a8b4896e640a4a462942482aeb88bf07da
prashanth chowdary <prashanthchowdary@prashanths-MacBook-Air.local>
2023-03-20 15:57:44 +0530

    The frist commit to know is github got the flie from the local git

---
+++ variables.js
@@ -0,0 +1,7 @@
+/* 
+Variables in JavaScript :
+
+In javascript most of the time we work with data, the "Variable" -> is a "named storage"
+which means we store the data in our own naming convention 
+
+*/
